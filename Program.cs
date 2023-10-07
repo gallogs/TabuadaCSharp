cf4253ac16d8ec19177fb8dd5feb4b709dce3e11
Matheus Gallo <gallomatheus.ti@gmail.com>
2023-10-07 12:34:10 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+//Tabuada:
+
+int resultado, numero;
+
+
+Console.WriteLine("Informe o número inteiro > \t");
+numero = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <=10; i++){
+    
+    resultado = numero * i ;
+    Console.WriteLine(numero + "x" + i + "=" + resultado);
+}
+
+Console.WriteLine("\n Fim do processamento......");
+Console.ReadKey();
+
+
